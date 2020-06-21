e676ebdbf98417dfa09cbd408f1c0c0a78a7e541
Miguel Merayo <contractor.miguel.merayo@esb.ie>
2020-06-21 17:53:05 +0100

    Added unit test of manual reset event async

--- src/S3.Threading.UnitTests/ManualResetEventAsyncTests.cs
+++ src/S3.Threading.UnitTests/ManualResetEventAsyncTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -16,25 +14,23 @@ namespace S3.Threading.UnitTests
 		public async Task CanExecute()
 		{
 			var target = new ManualResetEventAsync(false);
-			var actual= new ConcurrentBag<int>();
-			var task1 = Task.Factory.StartNew(async () => actual.Add(await _Execute(target,1)));
+			var actual = new ConcurrentBag<int>();
+			var task1 = Task.Factory.StartNew(async () => actual.Add(await _Execute(target, 1)));
 			var task2 = Task.Factory.StartNew(async () => actual.Add(await _Execute(target, 2)));
 			Thread.Sleep(TimeSpan.FromMilliseconds(500));
 			CollectionAssert.IsEmpty(actual);
-			
+
 			target.Set();
 			Thread.Sleep(TimeSpan.FromMilliseconds(500));
-			
 
-			CollectionAssert.AreEquivalent(Enumerable.Range(1,2),actual);
+			CollectionAssert.AreEquivalent(Enumerable.Range(1, 2), actual);
 			Assert.IsTrue(task1.IsCompletedSuccessfully);
 			Assert.IsTrue(task2.IsCompletedSuccessfully);
-
-
 		}
+
 		private async Task<int> _Execute(ManualResetEventAsync target, int id)
 		{
-		
+
 			await target.WaitAsync();
 			return id;
 		}
