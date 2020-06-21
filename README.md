7e6dbfe72c696648dbb72e9c74d2f086c053522b
SolidDev <mmerayo30@gmail.com>
2020-06-21 08:01:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<img src="https://github.com/SolidSoftwareServices/AutoBatcher/blob/master/docs/images/logo.png" width="200" height="200"/>
---
+++ src/S3.Threading.UnitTests/ManualResetEventAsyncTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace S3.Threading.UnitTests
+{
+	[TestFixture]
+	class ManualResetEventAsyncTests
+	{
+		[Test]
+		public async Task FirstTest()
+		{
+			Assert.Pass("TODO");
+		}
+	}
+}
