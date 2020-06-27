1e9ccc650b7e4ecc5f2f6e3f6b83795786a36abb
Miguel Merayo <contractor.miguel.merayo@esb.ie>
2020-06-27 05:31:17 +0100

    Added testing core

---
+++ src/S3.Testing.NUnit/UnitTestContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Moq;
+using Moq.AutoMock;
+
+namespace S3.Testing.NUnit
+{
+	/// <summary>
+	/// Base class for unit tests context. An specific unit tests object mother
+	/// </summary>
+	/// <typeparam name="TSut"></typeparam>
+	[DebuggerStepThrough]
+	public class UnitTestContext<TSut> : IDisposable
+		where TSut : class
+		{
+			public UnitTestContext():this(null)
+			{
+
+			}
+			public UnitTestContext(MockBehavior mockBehavior = MockBehavior.Default):this(null,mockBehavior)
+			{
+
+			}
+		public UnitTestContext(IFixture fixture = null):this(fixture,MockBehavior.Default)
+		{
+
+		}
+		public UnitTestContext(IFixture fixture = null,MockBehavior mockBehavior=MockBehavior.Default)
+		{
+			Fixture = fixture ?? new Fixture();
+			Fixture = Fixture.Customize(new AutoMoqCustomization());
+			AutoMocker = new AutoMocker(mockBehavior);
+		}
+
+		
+		private TSut _sut;
+		public AutoMocker AutoMocker { get; } 
+		public IFixture Fixture { get; }
+
+		/// <summary>
+		/// System Under Test. the target of the current unit tests
+		/// </summary>
+		public TSut Sut
+		{
+			get
+			{
+				if (_sut == null)
+				{
+					_sut = BuildSut(AutoMocker);
+				}
+				return _sut;
+			}
+		}
+
+		protected virtual TSut BuildSut(AutoMocker autoMocker)
+		{
+			return autoMocker.CreateInstance<TSut>();
+		}
+
+
+		public virtual void VerifyAllExpectations()
+		{
+			AutoMocker.VerifyAll();
+		}
+
+		public virtual void VerifyOnlyVerifiableExpectations()
+		{
+			AutoMocker.Verify();
+		}
+
+		public virtual void Dispose()
+		{
+		}
+	}
+}
---
+++ src/S3.Testing.NUnit/UnitTestFixture.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace S3.Testing.NUnit
+{
+
+	/// <summary>
+	/// Base class for all unit tests... From now on
+	/// </summary>
+	/// <typeparam name="TContext"></typeparam>
+	/// <typeparam name="TSut"></typeparam>
+	[TestFixture]
+	[Parallelizable(ParallelScope.Fixtures)]
+	public abstract class UnitTestFixture<TContext,TSut>
+		where TSut : class
+		where TContext:UnitTestContext<TSut>, new() 
+	{
+		protected TContext Context;
+		[SetUp]
+		public virtual void SetUp()
+		{
+			Context=new TContext();
+		}
+
+		[TearDown]
+		public virtual void TearDown()
+		{
+			Context.Dispose();
+		}
+	}
+
+}
